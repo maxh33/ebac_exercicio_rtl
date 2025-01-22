391e6701a8a640b5c4af0c71c1aead99baa8ee13
maxh33 <contact@maxhaider.dev>
2025-01-22 13:04:09 -0300

    Initial commit with project setup and basic components

--- src/components/PostComments/__tests__/PostComments.test.tsx
+++ src/components/PostComments/__tests__/PostComments.test.tsx
@@ -1,6 +1,6 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import Post from '..';
+import PostComment from '..';
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
